037b34c363e10e8b2598bc31d8920716aea48855
Richard Wasonga <rowwasonga@gmail.com>
2025-05-09 15:46:07 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+
+    if length == 0:
+        print(fib_list)
+        return
+
+    fib_list.append(0)
+    if length > 1:
+        fib_list.append(1)
+
+    for i in range(2,length):
+        fib_list.append(fib_list[i - 1] + fib_list[i - 2])
+    print(fib_list)
+
+print_fibonacci(9)
+   
+  
